7682140a58f6e13f252f48fdf188cce5d38bedfa
Karolis Blazevicius <karolis@foje.lt>
2019-03-29 20:22:38 +0200

    Create solution with console project.

---
+++ src/Vintin.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vintin.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
