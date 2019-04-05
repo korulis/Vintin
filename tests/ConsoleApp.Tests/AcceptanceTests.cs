cd3e228e362a37d8d497a0ee40076e52775f46f4
Karolis Blazevicius <karolis@foje.lt>
2019-04-05 20:33:57 +0300

    Refactor.

--- tests/ConsoleApp.Tests/AcceptanceTests.cs
+++ tests/ConsoleApp.Tests/AcceptanceTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using Discounts;
@@ -9,22 +11,28 @@ namespace ConsoleApp.Tests
     public class AcceptanceTests
     {
         private const string InputFilePath = "input.txt";
+        private const string OutputFilePath = "output.txt";
 
-        [Fact]
-        public void FreeShippingTestCase()
+        public static TheoryData<string, IDiscounter> CalculatorData =>
+            new TheoryData<string, IDiscounter>
+            {
+                {"free-shipping-output.txt" , new FullDiscounts()},
+                {"greedy-shipping-output.txt" , new NoDiscounts()},
+            };
+
+        [Theory]
+        [MemberData(nameof(CalculatorData))]
+        public void ShippingPriceCalculationTests(string expectedOutputFile, IDiscounter discounter)
         {
             //Arrange
-            var expectedDiscountedLines = File.ReadLines("free-shipping-output.txt").ToArray();
-            const string outputFilePath = "actual-free-shipping-output.txt";
-            var fileBasedShippingPriceCalculator = BuildFileBasedShippingPriceCalculator(
-                new FullDiscounts(),
-                outputFilePath);
+            var expectedDiscountedLines = File.ReadLines(expectedOutputFile).ToArray();
+            var fileBasedShippingPriceCalculator = BuildFileBasedShippingPriceCalculator(discounter);
 
             //Act
             fileBasedShippingPriceCalculator.Process(InputFilePath);
 
             //Assert
-            var actualDiscountedEntries = File.ReadLines(outputFilePath).ToArray();
+            var actualDiscountedEntries = File.ReadLines(OutputFilePath).ToArray();
             for (var i = 0; i < expectedDiscountedLines.Length; i++)
             {
                 Assert.Equal(expectedDiscountedLines[i], actualDiscountedEntries[i]);
@@ -32,37 +40,14 @@ namespace ConsoleApp.Tests
             Assert.Equal(expectedDiscountedLines, actualDiscountedEntries);
         }
 
-        [Fact]
-        public void GreedyShippingTestCase()
-        {
-            //Arrange
-            var expectedDiscountedLines = File.ReadLines("greedy-shipping-output.txt").ToArray();
-            const string outputFilePath = "actual-greedy-shipping-output.txt";
-            var fileBasedShippingPriceCalculator = BuildFileBasedShippingPriceCalculator(
-                new NoDiscounts(),
-                outputFilePath);
-
-            //Act
-            fileBasedShippingPriceCalculator.Process(InputFilePath);
-
-            //Assert
-            var actualDiscountedEntries = File.ReadLines(outputFilePath).ToArray();
-            for (var i = 0; i < expectedDiscountedLines.Length; i++)
-            {
-                Assert.Equal(expectedDiscountedLines[i], actualDiscountedEntries[i]);
-            }
-            Assert.Equal(expectedDiscountedLines, actualDiscountedEntries);
-        }
-        private static FileBasedShippingPriceCalculator BuildFileBasedShippingPriceCalculator(
-            IDiscounter discounter,
-            string outputFilePath)
+        private static FileBasedShippingPriceCalculator BuildFileBasedShippingPriceCalculator(IDiscounter discounter)
         {
             const string acceptableDateFormat = "yyyy-MM-dd";
             const string separator = " ";
 
             var shippingEntryParser = new ShippingEntryMapper(separator, acceptableDateFormat);
             var shippingCostCalculator = new ShippingPriceCalculator(discounter);
-            void OutputMethod(IEnumerable<string> x) => File.WriteAllLines(outputFilePath, x);
+            void OutputMethod(IEnumerable<string> x) => File.WriteAllLines(OutputFilePath, x);
 
             var fileBasedShippingPriceCalculator = new FileBasedShippingPriceCalculator(
                 shippingEntryParser,
