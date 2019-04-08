73d373bdb87f5b6577c955673e8cd2db1b7e3a36
Karolis Blazevicius <broliukas@gmail.com>
2019-04-08 21:59:39 +0300

    Refactor.

--- tests/ConsoleApp.Tests/ThirdLpPackageDiscounterBehavioralTests.cs
+++ tests/ConsoleApp.Tests/ThirdLpPackageDiscounterBehavioralTests.cs
@@ -14,23 +14,19 @@ namespace ConsoleApp.Tests
         private readonly ThirdLpPackageDiscounter _sut;
         private readonly Mock<DiscountingRules> _discountRules;
         private readonly IFixture _fixture;
-        private readonly Mock<ShipmentWithApplicableDiscount> _shipmentUnderDiscount;
 
         public ThirdLpPackageDiscounterBehavioralTests()
         {
             _fixture = new Fixture().Customize(new AutoMoqCustomization());
 
-            _shipmentUnderDiscount = _fixture.Freeze<Mock<ShipmentWithApplicableDiscount>>();
-            _shipmentUnderDiscount.Setup(t => t.Apply())
+            var shipmentUnderDiscount = _fixture.Freeze<Mock<ShipmentWithApplicableDiscount>>();
+            shipmentUnderDiscount.Setup(t => t.Apply())
                 .Returns(() => _fixture.Create<ShipmentCost>());
 
             _discountRules = _fixture.Freeze<Mock<DiscountingRules>>();
             _discountRules.Setup(x => x.AssignDiscount(It.IsAny<ShipmentCost>()))
-                .Returns(_shipmentUnderDiscount.Object);
-
+                .Returns(shipmentUnderDiscount.Object);
            
-
-
             var underlyingDiscounter = new Mock<Discounter>();
             underlyingDiscounter.Setup(t => t.Discount(It.IsAny<IEnumerable<ShipmentCost>>()))
                 .Returns<IEnumerable<ShipmentCost>>(x => x);
@@ -52,7 +48,6 @@ namespace ConsoleApp.Tests
             {
                 _discountRules.Verify(t=>t.AssignDiscount(shipmentsCost), Times.Once);
             }
-
         }
 
 
@@ -65,19 +60,18 @@ namespace ConsoleApp.Tests
             {
                 firstShipment
             };
+            var shipmentUnderDiscount = _fixture.Freeze<Mock<ShipmentWithApplicableDiscount>>();
             _discountRules.Setup(t => t.AssignDiscount(firstShipment))
-                .Returns(_shipmentUnderDiscount.Object);
-            var shipmentCost = _fixture.Create<ShipmentCost>();
-            _shipmentUnderDiscount.Setup(t => t.Apply())
-                .Returns(shipmentCost);
-
+                .Returns(shipmentUnderDiscount.Object);
 
             //Act
             _sut.Discount(input);
 
             //Assert
-            _shipmentUnderDiscount.Verify(t => t.Apply(), Times.Once);
+            shipmentUnderDiscount.Verify(t => t.Apply(), Times.Once);
         }
+
+
 
     }
 }
